79d48d258551829859380fb134eefd0872d93291
LiveInside <maksyta053@mail.ru>
2023-08-15 16:26:45 +0300

    init9

--- src/main/java/org/example/GCD.java
+++ src/main/java/org/example/GCD.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.apache.commons.math3.util.ArithmeticUtils;
 
+import java.util.Arrays;
 import java.util.Random;
 
 // Определение наибольшего общего делителя
@@ -10,21 +11,33 @@ public class GCD {
         String userName = Greet.greet();
         System.out.println("What is the greatest common divisor?");
 
-        for (var i = 0; i < 3; i++){
+        for (var i = 0; i < 3; i++) {
             Random random = new Random();
-            int randomNumber1 = random.nextInt(11);
-            int randomNumber2 = random.nextInt(10);
+            int sizeArray = random.nextInt(11);
+            int[] randomNumbers = new int[sizeArray];
 
-            System.out.println("Quastion: " + randomNumber1 + " " + randomNumber2);
+            System.out.print("Question: " );
+            for (int j = 0; j < sizeArray; j++) {
+                randomNumbers[j] = random.nextInt(101);
+                System.out.print(randomNumbers[j] + " ");
+            }
 
-            int trueAnswer = ArithmeticUtils.gcd(randomNumber1, randomNumber2);
+            int trueAnswer = findGcd(randomNumbers);
             int userAnswer = UserInput.userInput();
 
             if(EndOutput.endOutput(trueAnswer, userAnswer, userName)){
                 return;
             }
-
         }
         System.out.println("Congratulations, " + userName);
+    }
+
+    private static int findGcd(int[] randomNumbers) {
+        int gcd = randomNumbers[0];
+        for (int i = 1; i < randomNumbers.length; i++) {
+            gcd = ArithmeticUtils.gcd(gcd, randomNumbers[i]);
+        }
+        System.out.println();
+        return gcd;
     }
 }
